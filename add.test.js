f018b90155c0021eee7769ed73dc8bf6e4e1f071
Stoyan Kolev <slownessbg@gmail.com>
2023-06-12 16:42:52 +0300

    my failing commit

--- add.test.js
+++ add.test.js
@@ -3,3 +3,7 @@ const add = require('./add.js').add;
 test('add properly adds two numbers', () => {
     expect(add(1, 2)).toEqual(3);
 }); 
+
+test('my test that will fail', () => {
+    expect(add(1,2)).toEqual(2);
+})
